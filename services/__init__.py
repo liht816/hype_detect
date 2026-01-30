87074be657165719f7497b5d8cb0e0f372a522e6
liht816 <botesasswww@gmail.com>
2026-01-30 12:18:08 +0700

    Update __init__.py

--- services/__init__.py
+++ services/__init__.py
@@ -2,8 +2,6 @@
 Экспорт сервисов
 """
 from services.coingecko import CoinGeckoService
-from services.reddit_analyzer import RedditAnalyzer
-from services.twitter_analyzer import TwitterAnalyzer
 from services.google_trends import GoogleTrendsService
 from services.whale_tracker import WhaleTracker
 from services.fear_greed_api import FearGreedService
@@ -12,8 +10,6 @@ from services.github_analyzer import GitHubAnalyzer
 
 __all__ = [
     "CoinGeckoService",
-    "RedditAnalyzer",
-    "TwitterAnalyzer",
     "GoogleTrendsService",
     "WhaleTracker",
     "FearGreedService",
